de96806fe973a8b4c9b0593356c44fa104210940
Will Forbes <42383119+WilliamForbesJr@users.noreply.github.com>
2020-04-07 09:19:55 -0400

    Add cart component and cart dropdown component * add reducers and actions for display value for component

---
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import CustomButton from '../custom-button/custom-button.component.js';
+
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = () => (
+  <div className='cart-dropdown'>
+    <div className='cart-items' />
+    <CustomButton>Go to Checkout</CustomButton>
+  </div>
+);
+
+export default CartDropdown;
