22b37e40bd75abf88fd6a65c55c4a94bf8485aea
Will Forbes <42383119+WilliamForbesJr@users.noreply.github.com>
2020-05-21 17:19:56 -0400

    * add styling for quantity buttons * add removeItem reducer and actions * added functionality to checkout-item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,22 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 };
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  // find matching cartItem to adjust quantity
+  const existingItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  );
+  
+  // remove item completely if only 1 left
+  if (existingItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+  };
+
+  // reduce quantity in cart
+  return cartItems.map(cartItem => 
+    cartItem.id === cartItemToRemove.id 
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  )
+}
