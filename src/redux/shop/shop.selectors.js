869b747766cf76a3878014321da71e1e60ebab02
Will Forbes <42383119+WilliamForbesJr@users.noreply.github.com>
2020-05-26 12:09:42 -0400

    * refactored collection data to be obj instead of    array. This is just a cleaner way to store/find data * fixed bug where collectionId wasn't being passed   into the collectionUrlParam * styling fixes

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -6,3 +6,12 @@ export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
 );
+
+/* used to match collection id number with COLLETION_ID_MAP key. 
+ * This determines route in shop component
+*/
+export const selectCollection = collectionUrlParam => 
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+  )
